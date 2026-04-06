c4e68bb7fa2c96533f558235734bf1ef5186d7d9
Stefan Thor Bjarnason <stefanttb@gmail.com>
2026-04-06 16:36:16 +0000

    better.

--- setup-pop-24.sh
+++ setup-pop-24.sh
@@ -398,7 +398,7 @@ install_bob() {
 install_basic_packages
 install_bitwarden
 export SSH_AUTH_SOCK=$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock
-read -rsn1 -p "Start Bitwarden and press any key to continue..."
+read -rsn1 -p "Start Bitwarden and press any key to continue..." < /dev/tty
 
 install_dotfiles
 install_bash_dotfiles
